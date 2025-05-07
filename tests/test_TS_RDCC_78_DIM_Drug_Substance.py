a2f6c70bab6564bdae7ce80de91d9b043c41fa8f
JHA SHILPI -DS <L100541-DS@llynet.com>
2025-05-07 19:39:13 +0530

    Added tests

--- tests/test_TS_RDCC_78_DIM_Drug_Substance.py
+++ tests/test_TS_RDCC_78_DIM_Drug_Substance.py
@@ -55,7 +55,7 @@ def test_TS_RDCC_78_TC_RDCC_79_data_completeness(db_connection: connection | Non
         tgt_table=validation['target_table'],
         src_cols=['concept_code'],
         tgt_cols=['ds_material_number'],
-        src_filter=f"{validation['source_schema']}.{validation['source_table']}.vocabulary_name='DS Flavour'",
+        src_filter=f"{validation['source_schema']}.{validation['source_table']}.vocabulary_name='DS Flavor'",
         tgt_filter=""
     )
 
@@ -70,11 +70,99 @@ def test_TS_RDCC_78_TC_RDCC_79_data_completeness(db_connection: connection | Non
         src_cols=['concept_code'],
         tgt_cols=['ds_material_number'],
         src_filter=f"""{validation['source_schema']}.{validation['source_table']}.
-        vocabulary_name='DS Flavour'""",
+        vocabulary_name='DS Flavor'""",
         tgt_filter=""
     )
     print(msg)
     assert result, msg
+
+def test_TS_RDCC_78_TC_RDCC_79_null_check(db_connection, validation):
+    print("\nIdentify there is no Null values for ds_material_number in dim table.\n")
+    columns_to_check = ["ds_material_number"]
+    result = check_columns_for_nulls(db_connection, validation['target_schema'], validation['target_table'], columns_to_check)
+
+    for col, null_count in result.items():
+        message = f"✅ Column '{col}' has no NULL values." if null_count == 0 else f"❌ Column '{col}' contains {null_count} NULL values."
+        print(message)
+        assert null_count == 0, message
+
+def test_TS_RDCC_78_TC_RDCC_80_process_identifier(db_connection: connection | None,validation: dict[str, str]):
+    print("\nIdentify process_identfier in the dim_regcor_drug_substance table that are missing in the source table (dim_rdm_regcor_master_table): \n")
+    query =f"""SELECT tgt.process_identifier 
+    FROM {validation['target_schema']}.{validation['target_table']} tgt
+    except
+    SELECT 
+    MAX(CASE WHEN property_name = 'Process Identifier' THEN property_value END) AS process_identifier
+        FROM {validation['source_schema']}.{validation['source_table']}
+        WHERE vocabulary_name = 'DS Flavor'
+    group by concept_code"""
+    test, diff_count , message  = run_and_validate_empty_query(db_connection, query, "Data Completeness Check")
+    
+    try:
+        if diff_count == 0:
+            message = f"✅ Target-to-Source check passed: All records from {validation['target_table']} exist in {validation['source_table']}."
+            logging.info(message)
+            test = True
+        else:
+            message = f"❌ Target-to-Source check failed: {diff_count} records in {validation['target_table']} missing from {validation['source_table']}."
+            logging.error(message)
+            test = False
+
+    except Exception as e:
+        message = f"❌ Error during target-to-source completeness validation: {str(e)}"
+        logging.exception(message)
+        test = False
+        
+    assert test,message
+    print(message)
+
+    query =f"""SELECT 
+        MAX(CASE WHEN property_name = 'Process Identifier' THEN property_value END) 
+        AS process_identifier
+        FROM {validation['source_schema']}.{validation['source_table']}
+        WHERE vocabulary_name = 'DS Flavor'
+        group by concept_code 
+    except 
+    SELECT tgt.process_identifier 
+    FROM {validation['target_schema']}.{validation['target_table']} tgt"""
+    
+    test, diff_count , message  = run_and_validate_empty_query(db_connection, query, "Data Completeness Check")
+    
+    try:
+        if diff_count == 0:
+            message = f"✅ Source-to-Target check passed: All records from {validation['source_table']} exist in {validation['target_table']}."
+            logging.info(message)
+            test = True
+        else:
+            message = f"❌ Source-to-Target check failed: {diff_count} records in {validation['source_table']} missing from {validation['target_table']}."
+            logging.error(message)
+            test = False
+
+    except Exception as e:
+        message = f"❌ Error during Source-to-Target completeness validation: {str(e)}"
+        logging.exception(message)
+        test = False
+        
+    assert test,message
+    print(message)
+
+def test_TS_RDCC_78_TC_RDCC_79_null_check(db_connection, validation):
+    print("\nIdentify there is no Null values for process_identifier in dim table.\n")
+    columns_to_check = ["process_identifier"]
+    result = check_columns_for_nulls(db_connection, validation['target_schema'], validation['target_table'], columns_to_check)
+
+    for col, null_count in result.items():
+        message = f"✅ Column '{col}' has no NULL values." if null_count == 0 else f"❌ Column '{col}' contains {null_count} NULL values."
+        print(message)
+        assert null_count == 0, message
+
+
+
+
+
+
+
+
 
 
 
