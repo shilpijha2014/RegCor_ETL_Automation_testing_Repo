5474e42fbf4ea94e9803396e57d5ae67291f7e83
JHA SHILPI -DS <L100541-DS@llynet.com>
2025-05-14 13:35:30 +0530

    Added DIM tests

--- tests/test_TS_RDCC_196_DCC DP-DA Association.py
+++ tests/test_TS_RDCC_196_DCC DP-DA Association.py
@@ -20,17 +20,15 @@ def validation():
         "source_table": "dim_rdm_regcor_master_table",   
         "target_db": "regcor_refine_db" ,
         "target_schema": "regcor_refine",        
-        "target_table": "stg_dim_regcor_container_closure_system"
+        "target_table": "stg_association_regcor_dp_da"
     }
-
-#This Test set includes test cases of DIM master RDM.
 
 def test_validate_connection(db_connection: connection | None, validation: dict[str, str]):
     """
     Test to validate that a connection to the database can be established.
     """
     try:
-        print(f"\nTest Set-RDCC- 202 - This test set contains of test cases of dim_regcor_container_closure_system table.\n")
+        print(f"\nTest Set-RDCC- 196 - This test set contains of test cases of Association_regcor_dp_da table.\n")
         
         assert db_connection is not None, f"❌ Connection object is None for {validation['target_db']}"
         print(f"✅ Successfully connected to database: {validation['target_db']}")
@@ -43,27 +41,28 @@ def test_table_exists(db_connection: connection | None,validation: dict[str, str
     assert validate_table_exists( db_connection,validation["target_schema"], validation["target_table"]), "❌ Target Table does not exist!"
     print(f"\nTable {validation["target_table"]} exists.")
 
-def test_TS_RDCC_202_TC_RDCC_203_container_closure_system_name_validation(db_connection: connection | None,validation: dict[str, str]): 
-    print(f"\nThis test case verifies that container_closure_system_name and container_closure_system_description in dim_regcor_container_closure_system are correctly retrieved using concept_code and concept_name from the source table dim_rdm_regcor_master, with the filter Vocabulary_name set to 'Container Closure System'\n")
-    print(f"Test 1 : Identify records in the dim_regcor_container_closure_system table that are missing in the source table (dim_rdm_regcor_master_table):\n")
-    query =f"""SELECT  
-                s.container_closure_system_name,
-                    s.container_closure_system_description 
-                FROM {validation['target_schema']}.{validation['target_table']} s
-                LEFT JOIN (
-                    SELECT 
-                        concept_code AS container_closure_system_name, 
-                        concept_name AS container_closure_system_description 
-                    FROM {validation['source_schema']}.{validation['source_table']}
-                    WHERE 
-                        vocabulary_name = 'Container Closure System'
-                    GROUP BY 
-                        concept_code,concept_name
-                ) src
-                ON s.container_closure_system_name = src.container_closure_system_name 
-                AND s.container_closure_system_description = src.container_closure_system_description
-                WHERE src.container_closure_system_name IS NULL 
-                OR src.container_closure_system_description IS NULL"""
+def test_TS_RDCC_196_TC_RDCC_197_dp_material_da_material_validation(db_connection: connection | None,validation: dict[str, str]): 
+    print(f"\nThis test case validates that da_material_number and dp_material_number in Association_regcor_da_dp are correctly fetched using concept_code and relation_code_to from the source table dim_rdm_regcor_master, with the filters Vocabulary_name set to 'DA Flavor' and relation_name set to 'DP Flavor'.\n")
+    print(f"Test 1 : Identify records in the association_regcor_da_dp table that are missing in the source table (dim_rdm_regcor_master_table):\n")
+    query =f"""SELECT 
+    s.da_material_number, 
+    s.dp_material_number 
+        FROM {validation['target_schema']}.{validation['target_table']} s
+        LEFT JOIN (
+            SELECT 
+                concept_code AS da_material_number, 
+                relation_code_to AS dp_material_number 
+            FROM {validation['source_schema']}.{validation['source_table']}
+            WHERE 
+                vocabulary_name = 'DA Flavor'
+                AND relation_name = 'DP Flavor'
+            GROUP BY 
+                concept_code, relation_code_to
+        ) src
+        ON s.da_material_number = src.da_material_number 
+        AND s.dp_material_number = src.dp_material_number
+        WHERE src.da_material_number IS NULL 
+        OR src.dP_material_number IS NULL;"""
 
     test, diff_count , message  = run_and_validate_empty_query(db_connection, query, "Data Completeness Check")
     
@@ -85,27 +84,27 @@ def test_TS_RDCC_202_TC_RDCC_203_container_closure_system_name_validation(db_con
     assert test,message
     print(message)
 
-    print(f"\n2)Identify records in the source table that are missing in the dim_regcor_container_closure_system table:")
-
+    print(f"\n2)Identify records in the source table that are missing in the association_regcor_dp_da table:\n")
     query =f"""SELECT 
-    src.container_closure_system_name, 
-    src.container_closure_system_description 
+    src.da_material_number, 
+    src.dp_material_number 
         FROM (
             SELECT 
-                concept_code AS container_closure_system_name, 
-                relation_code_to AS container_closure_system_description 
+                concept_code AS da_material_number, 
+                relation_code_to AS dp_material_number 
             FROM {validation['source_schema']}.{validation['source_table']}
             WHERE 
-                vocabulary_name = 'Container Closure System'
+                vocabulary_name = 'DA Flavor'
+                AND relation_name = 'DP Flavor'
             GROUP BY 
                 concept_code, relation_code_to
         ) src
         LEFT JOIN {validation['target_schema']}.{validation['target_table']} s
-        ON s.container_closure_system_name = src.container_closure_system_name 
-        AND s.container_closure_system_description = src.container_closure_system_description
-        WHERE src.container_closure_system_name IS NULL 
-        and src.container_closure_system_description IS NULL;
- """
+        ON s.da_material_number = src.da_material_number 
+        AND s.dp_material_number = src.dp_material_number
+        WHERE s.da_material_number IS NULL 
+        OR s.dp_material_number IS NULL;
+        """
     test, diff_count , message  = run_and_validate_empty_query(db_connection, query, "Data Completeness Check")
     
     try:
@@ -126,11 +125,11 @@ def test_TS_RDCC_202_TC_RDCC_203_container_closure_system_name_validation(db_con
     assert test,message
     print(message)
 
-def test_TS_RDCC_202_TC_RDCC_204_PK_Check(db_connection: connection | None,validation: dict[str, str]):
-    print("\nThis test case validates the duplicates and null checks for the primary key columns container_closure_system_name and container_closure_system_description in dim_regcor_container_closure_system.\n")
+def test_TS_RDCC_196_TC_RDCC_198_PK_Check(db_connection: connection | None,validation: dict[str, str]):
+    print("\nThis test case validates the duplicates and null checks for the primary key columns ds_material_number and dp_material_number in Association_assembly_ds_dp.\n")
     # -- Check for duplicates in primary keys 
     print(f"1.Check for Duplicates\n")
-    primary_keys = ['container_closure_system_name'  ,'container_closure_system_description']
+    primary_keys = [ 'da_material_number' ,'dp_material_number']
     result = check_primary_key_duplicates(
     connection=db_connection,
     schema_name=validation['target_schema'],
@@ -161,42 +160,46 @@ def test_TS_RDCC_202_TC_RDCC_204_PK_Check(db_connection: connection | None,valid
     assert test,message
     print(message)
 
-def test_TS_RDCC_202_TC_RDCC_208_No_Source_Value_Transformation_Check(db_connection: connection | None,validation: dict[str, str]):
-    print("\nThis test case ensures that the columns in the dim_product_family_item_configuration table are transformed to no_source_values when null values are present in the source table.\n")
+def test_TS_RDCC_199_TC_RDCC_206_No_Source_Value_Transformation_Check(db_connection: connection | None,validation: dict[str, str]):
+    print("\nThis test case validates that columns in the Association_dp_da table are transformed to no_source_values when null values are present in the source table.\n")
     query =f"""
-        WITH source_data AS (select 
-distinct 
-coalesce(concept_code, 
-'No_Source_Value') 
-as container_closure_system_name, 
-coalesce(concept_name, 
-'No_Source_Value') 
-as container_closure_system_description 
-from 
-{validation['source_schema']}.{validation['source_table']}
-where 
-vocabulary_name = 'Container Closure System'),
-target_data AS (
-    SELECT 
-       container_closure_system_name,container_closure_system_description
-    FROM 
-        {validation['target_schema']}.{validation['target_table']}
-)
-SELECT 
-s.container_closure_system_name AS source_container_closure_system_name,
-    t.container_closure_system_name AS targetcontainer_closure_system_name,
-    s.container_closure_system_description as source_container_closure_system_description,
-t.container_closure_system_description as target_container_closure_system_description
-FROM 
-    source_data s
-LEFT JOIN 
-    target_data t
-ON 
-    s.container_closure_system_name = t.container_closure_system_name
-    and s.container_closure_system_description=t.container_closure_system_description
-where 
-s.container_closure_system_description!=t.container_closure_system_description
-or s.container_closure_system_name!=t.container_closure_system_name"""
+        WITH source_data AS (
+            SELECT 
+                COALESCE(concept_code, 'No_Source_Value') AS da_material_number, 
+                COALESCE(relation_code_to, 'No_Source_Value') AS dp_material_number 
+            FROM 
+               {validation['source_schema']}.{validation['source_table']} 
+            WHERE 
+                vocabulary_name = 'DA Flavor' 
+                AND relation_name = 'DP Flavor' 
+            GROUP BY 
+                concept_code, 
+                relation_code_to
+        ),
+        target_data AS (
+            SELECT 
+                da_material_number, 
+                dp_material_number 
+            FROM 
+                {validation['target_schema']}.{validation['target_table']}
+        )
+        -- Compare source and target data
+        SELECT 
+            s.da_material_number AS source_da_material_number,
+            t.da_material_number AS target_da_material_number,
+            s.dp_material_number AS source_dp_material_number,
+            t.dp_material_number AS target_dp_material_number
+        FROM 
+            source_data s
+        LEFT JOIN 
+            target_data t
+        ON 
+            s.da_material_number = t.da_material_number
+            AND s.dp_material_number = t.dp_material_number
+        WHERE 
+            t.da_material_number IS NULL 
+            OR t.dp_material_number IS NULL;"""
+    
     test, diff_count , message  = run_and_validate_empty_query(db_connection, query, "Data Completeness Check")
     
     try:
